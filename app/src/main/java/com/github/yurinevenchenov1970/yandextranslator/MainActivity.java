2672cf27d327711bb34db68ff601a81506a5013d
Yuri Nevenchenov <yurinevenchenov1970@gmail.com>
2017-10-05 13:41:05 +0300

    Initial commit

---
+++ app/src/main/java/com/github/yurinevenchenov1970/yandextranslator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.yurinevenchenov1970.yandextranslator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
