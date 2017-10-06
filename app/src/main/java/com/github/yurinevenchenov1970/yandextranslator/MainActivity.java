04d97dad5d2cb6f23788227846432c9053860104
Yuri Nevenchenov <yurinevenchenov1970@gmail.com>
2017-10-06 17:32:45 +0300

    added ui components via butterKnife

--- app/src/main/java/com/github/yurinevenchenov1970/yandextranslator/MainActivity.java
+++ app/src/main/java/com/github/yurinevenchenov1970/yandextranslator/MainActivity.java
@@ -1,13 +1,35 @@
 package com.github.yurinevenchenov1970.yandextranslator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.input_edit_text)
+    EditText mInputEditText;
+
+    @BindView(R.id.progress_bar)
+    ProgressBar mProgressBar;
+
+    @BindView(R.id.translation_text_view)
+    TextView mTranslationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.translate_button)
+    void translate() {
+        //here we go to handle button click
     }
 }
